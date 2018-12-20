6cb8f941f796b3c8f3123a179a633ae9443358ef
Luke Lin <milletbard@gmail.com>
2018-12-20 18:05:36 +0800

    add

---
+++ precache-manifest.101e5fd8f0305733c9fae1d7f00ec32b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "653c5ca003c3e0cc45a4",
+    "url": "/React-template/static/js/runtime~main.653c5ca0.js"
+  },
+  {
+    "revision": "a28bc41e5f0a1fc64a65",
+    "url": "/React-template/static/js/main.a28bc41e.chunk.js"
+  },
+  {
+    "revision": "a352a67de7b7d6dabc94",
+    "url": "/React-template/static/js/1.a352a67d.chunk.js"
+  },
+  {
+    "revision": "a28bc41e5f0a1fc64a65",
+    "url": "/React-template/static/css/main.a5b57bf1.chunk.css"
+  },
+  {
+    "revision": "1dd617b8c8c6f4c3f66c6e2452d0a21f",
+    "url": "/React-template/index.html"
+  }
+];
